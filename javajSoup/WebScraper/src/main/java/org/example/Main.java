921516619fb0d8956827d8d323dee1140c508ac3
KutluayLav <kutluay3344@hotmail.com>
2024-05-27 04:32:40 +0300

    feat:txt file

--- javajSoup/WebScraper/src/main/java/org/example/Main.java
+++ javajSoup/WebScraper/src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -9,18 +11,41 @@ import javax.net.ssl.*;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 
 public class Main {
     public static void main(String[] args) {
 
-        disableCertificateValidation();
+        try {
+            // JSON dosyasını oku
+            String jsonContent = new String(Files.readAllBytes(Paths.get("new_data_68.json")));
+
+            // JSON dizgesini JSONArray nesnesine dönüştür
+            JSONArray jsonArray = new JSONArray(jsonContent);
+
+            // Yalnızca 'text' alanını içeren metin dosyasına yaz
+            FileWriter writer = new FileWriter("veri.txt");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                String text = jsonObject.getString("text");
+                writer.write(text + "\n\n"); // Her metin öğesinden sonra bir boş satır ekleyin
+            }
+            writer.close();
+
+            System.out.println("JSON verisi başarıyla TXT dosyasına yazıldı.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    /*    disableCertificateValidation();
         https://www.firat.edu.tr/tr/page/news?page=2
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter("duyuru.txt", true));
 
-            for (int page = 35; page <= 40; page++) { // 30 sayfaya kadar döngü oluştur
+            for (int page = 35; page <= 40; page++) {
                 String baseUrl = "https://www.firat.edu.tr/tr/page/news?page=" + page;
                 Document mainPage = Jsoup.connect(baseUrl).get();
                 Elements posts = mainPage.select(".item-content  a");
@@ -48,9 +73,9 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
+    } */
 
-    private static void disableCertificateValidation() {
+   /* private static void disableCertificateValidation() {
         try {
             TrustManager[] trustAllCerts = new TrustManager[]{
                     new X509TrustManager() {
@@ -76,6 +101,6 @@ public class Main {
             HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
         } catch (NoSuchAlgorithmException | KeyManagementException e) {
             e.printStackTrace();
-        }
+        } */
     }
 }
